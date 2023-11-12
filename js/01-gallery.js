e642939c8e3f2e9ca451a2bc15d6ee2e27921494
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-11-12 19:49:24 +0200

    add structure

---
+++ js/01-gallery.js
@@ -0,0 +1,9 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const list = document.querySelector('.gallery');
+
+console.dir(list)
